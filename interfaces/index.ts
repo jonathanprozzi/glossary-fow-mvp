acc05837de1255728e890030554e360405dc18a6
jonathanprozzi <jonathanprozzi@gmail.com>
2020-07-01 14:13:17 -0400

    added Glossary type to interfaces, began adding in the data into sample-data.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -4,7 +4,9 @@
 //
 // import User from 'path/to/interfaces';
 
-export type User = {
-  id: number
-  name: string
-}
+export type Glossary = {
+  id: number;
+  term: string;
+  definition: string;
+  week: number;
+};
--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -1,9 +1,12 @@
-import { User } from '../interfaces'
+import { Glossary } from "../interfaces";
 
-/** Dummy user data. */
-export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
-]
+/** Glosary Data **/
+export const glossaryData: Glossary[] = [
+  {
+    id: 100,
+    term: "(User) Profile",
+    definition:
+      "The public - facing information about yourself you put online for any program.",
+    week: 1,
+  },
+];
