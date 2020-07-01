f318e1d634fbabc585f32ec09e1cf2e854d6c5af
jonathanprozzi <jonathanprozzi@gmail.com>
2020-07-01 15:18:09 -0400

    added margin/padding to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { Flex } from "@chakra-ui/core";
+import { Flex, Box } from "@chakra-ui/core";
 import GlossaryCard from "../components/GlossaryCard";
 
 const IndexPage = () => {
   return (
     <Flex direction="column" justify="center" align="center" gridArea="main">
-      <GlossaryCard />
+      <Box maxWidth="960px" margin="0 auto" paddingX={4} paddingY={4}>
+        <GlossaryCard />
+      </Box>
     </Flex>
   );
 };
