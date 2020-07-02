79a80faacab7fa2942c592f806c3f6ade6fc9757
jonathanprozzi <jonathanprozzi@gmail.com>
2020-07-02 11:52:35 -0400

    terms for group 1 loaded into data

--- utils/glossary-data.ts
+++ utils/glossary-data.ts
@@ -18,4 +18,140 @@ export const glossaryData: Glossary[] = [
     week: 1,
     group: "General",
   },
+  {
+    id: 102,
+    name: "Collaborating",
+    definition: "Working with more than one person on a project or idea.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 103,
+    name: "Desktop",
+    definition:
+      "The “home screen” of a computer. This is where you can find icons for programs on the device. ",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 104,
+    name: "Files",
+    definition:
+      "Items like documents, presentations, pictures, videos, folders that are saved to a computer or Cloud service like Google Drive. ",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 105,
+    name: "Google Calendar",
+    definition:
+      "A free program part of the GSuite that allows the user to create and organize and send invitations.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 106,
+    name: "Google Chrome",
+    definition: "A Google-powered web browser.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 107,
+    name: "Google Docs",
+    definition:
+      "A free program part of the GSuite for creating text documents.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 108,
+    name: "Google Meet",
+    definition:
+      "A free program part of the GSuite for organizing and participating in video calls.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 109,
+    name: "Google Sheets",
+    definition: "A free program part of the GSuite for creating spreadsheets.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 110,
+    name: "Google Slides",
+    definition: "A free program part of the GSuite for creating presentations.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 111,
+    name: "Interface",
+    definition:
+      "How you interact with a program. This includes the tool bar, functions and abilities of a program.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 112,
+    name: "Operating System (OS)",
+    definition:
+      "The software that runs a computer. Some common OSs are Windows and Mac.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 113,
+    name: "Permissions",
+    definition:
+      "The access granted by you or other users to see personal information.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 114,
+    name: "Screen Readers",
+    definition: "Programs that read out the text on a computer screen.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 115,
+    name: "Slack",
+    definition: "A program for instant messaging across teams.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 116,
+    name: "Teleworking",
+    definition:
+      "Completing work with the help of the internet or phone to collaborate with people in other locations.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 117,
+    name: "Web Browser",
+    definition:
+      "Program for accessing the internet. A few examples include Firefox, Chrome, Edge, and Safari.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 118,
+    name: "Workspace",
+    definition: "A designated place to do work.",
+    week: 1,
+    group: "General",
+  },
+  {
+    id: 119,
+    name: "Zoom",
+    definition: "A paid video conferencing platform.",
+    week: 1,
+    group: "General",
+  },
 ];
