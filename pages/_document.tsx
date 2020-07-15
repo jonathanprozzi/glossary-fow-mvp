0d79d39db5984caee684df647adba88b00105d65
jonathanprozzi <jonathanprozzi@gmail.com>
2020-07-15 12:40:13 -0400

    removed unnecessary comment for ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${gaID}`}
